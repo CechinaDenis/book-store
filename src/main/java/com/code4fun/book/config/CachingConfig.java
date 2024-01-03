c29f1941b288ef13a3e1a6493ddbb144973e42ed
Denis Cechina <denis.cechina@seavus.com>
2024-01-03 11:42:06 +0000

    Added caching config. closes #29

---
+++ src/main/java/com/code4fun/book/config/CachingConfig.java
@@ -0,0 +1,8 @@
+package com.code4fun.book.config;
+
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@CacheConfig
+public class CachingConfig {}
